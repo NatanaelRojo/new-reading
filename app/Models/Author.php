bcbfa0951d62a1611e2efe3c7c20a40c649f9773
natanael <rojonatanael99@gmail.com>
2024-10-02 12:06:19 -0400

    feature(Implementing basic Author model): Implementing a basic author model

---
+++ app/Models/Author.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Author extends Model
+{
+    use HasFactory;
+    use HasSlug;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'nationality',
+        'biography',
+        'image_url',
+        'slug',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom(['first_name', 'last_name'])
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
