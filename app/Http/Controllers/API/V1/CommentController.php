7b55ccb10de3afecde9d5b621e1829111f6e8a9f
natanael <rojonatanael99@gmail.com>
2025-02-04 09:51:19 -0400

    feature(Creating Comment controller): Implementing a basic Comment controller

---
+++ app/Http/Controllers/API/V1/CommentController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Resources\API\V1\Comment\CommentResource;
+use App\Models\API\V1\Comment;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CommentController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        $comments = Comment::all();
+
+        return response()
+            ->json(CommentResource::collection($comments), JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $newComment = Comment::query()
+            ->create($request->validated());
+
+        return response()
+            ->json(new CommentResource($newComment), JsonResponse::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comment $comment): JsonResponse
+    {
+        return response()
+            ->json(new CommentResource($comment), JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment): JsonResponse
+    {
+        $comment->update($request->validated());
+
+        return response()
+            ->json(new CommentResource($comment), JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $comment): JsonResponse
+    {
+        $comment->delete();
+
+        return response()->json(null, JsonResponse::HTTP_NO_CONTENT);
+    }
+}
