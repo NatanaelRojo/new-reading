414a59c5daf9a2f1d850dd9575e8dcbb96a1abd2
natanael <rojonatanael99@gmail.com>
2025-02-04 10:30:49 -0400

    feature(Creating store and update requests for comments): Implementing a basic store and update request for Comment controllerr

---
+++ app/Http/Requests/API/V1/Comment/StoreCommentRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Comment;
+
+use App\Models\API\V1\Book;
+use App\Models\User;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class StoreCommentRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => ['required', 'integer', 'exists:' . User::class . ',id'],
+            'book_id' => ['required', 'integer', 'exists:' . Book::class . ',id'],
+            'body' => ['required', 'string', 'min:5', 'max:100'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors(),
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
---
+++ app/Http/Requests/API/V1/Comment/UpdateCommentRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Comment;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class UpdateCommentRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'body' => ['string', 'min:5', 'max:100'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors(),
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
