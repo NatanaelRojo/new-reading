27b9fcd46d121b06a25b597273e7068e8853413a
natanael <rojonatanael99@gmail.com>
2025-02-04 14:13:43 -0400

    feature(Applying Author, Tag and Book seeders)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AuthorSeeder::class,
+            TagSeeder::class,
+            BookSeeder::class,
             UserSeeder::class,
         ]);
     }
