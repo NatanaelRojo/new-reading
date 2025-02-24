8174c4126ea836ccf44469968506ca1ba127a1a9
natanael <rojonatanael99@gmail.com>
2025-02-23 21:26:53 -0400

    feature(Adding the published_at and image_url attributes to Book resource)

--- app/Http/Resources/API/V1/Book/BookResource.php
+++ app/Http/Resources/API/V1/Book/BookResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources\API\V1\Book;
 
 use App\Http\Resources\API\V1\Author\AuthorResource;
+use App\Http\Resources\API\V1\Genre\GenreResource;
+use App\Http\Resources\API\V1\Tag\TagResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -21,7 +23,11 @@ class BookResource extends JsonResource
             'isbn' => $this->isbn,
             'pages_amount' => $this->pages_amount,
             'chapters_amount' => $this->chapters_amount,
+            'published_at' => $this->published_at,
+            'image_url' => $this->image_url,
             'authors' => AuthorResource::collection($this->whenLoaded('authors')),
+            'genres' => GenreResource::collection($this->whenLoaded('genres')),
+            'tags' => TagResource::collection($this->whenLoaded('tags')),
         ];
 
     }
