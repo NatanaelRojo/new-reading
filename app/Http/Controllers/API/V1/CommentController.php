8338893bf1da2308d0887efc6690a00db87f7235
natanael <rojonatanael99@gmail.com>
2025-03-09 14:19:06 -0400

    feature(Adding methods to store comments for a post or a review): Implementing the methods indexByPost, indexByReview, storeByPost, and storeByReview to store a new commento for Post or Review

--- app/Http/Controllers/API/V1/CommentController.php
+++ app/Http/Controllers/API/V1/CommentController.php
@@ -6,6 +6,9 @@ use App\Http\Requests\API\V1\Comment\StoreCommentRequest;
 use App\Http\Requests\API\V1\Comment\UpdateCommentRequest;
 use App\Http\Resources\API\V1\Comment\CommentResource;
 use App\Models\API\V1\Comment;
+use App\Models\API\V1\Post;
+use App\Models\API\V1\Review;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -32,6 +35,78 @@ class CommentController
 
         return response()
             ->json(new CommentResource($newComment), JsonResponse::HTTP_CREATED);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param \App\Http\Requests\API\V1\Comment\StoreCommentRequest $request
+     * @param \App\Models\API\V1\Post $post
+     * @return JsonResponse|mixed
+     */
+    public function storeByPost(StoreCommentRequest $request, Post $post): JsonResponse
+    {
+        $validatedData = $request->validated();
+        $validatedData['post_id'] = $post->id;
+        $user = User::query()->firstWhere('id', $validatedData['user_id']);
+
+        if (!$user->isFollowing($post->user)) {
+            return response()
+                ->json('User not following author', JsonResponse::HTTP_CONFLICT);
+        }
+
+        $newComment = $post->comments()->create($validatedData);
+
+        return response()
+            ->json(new CommentResource($newComment), JsonResponse::HTTP_CREATED);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param \App\Http\Requests\API\V1\Comment\StoreCommentRequest $request
+     * @param \App\Models\API\V1\Review $review
+     * @return JsonResponse|mixed
+     */
+    public function storeByReview(StoreCommentRequest $request, Review $review): JsonResponse
+    {
+        $validatedData = $request->validated();
+        $validatedData['review_id'] = $review->id;
+        $user = User::query()->firstWhere('id', $validatedData['user_id']);
+
+        if (!$user->isFollowing($review->user)) {
+            return response()
+                ->json('User not following author', JsonResponse::HTTP_CONFLICT);
+        }
+
+        $newComment = $review->comments()->create($validatedData);
+
+        return response()
+            ->json(new CommentResource($newComment), JsonResponse::HTTP_CREATED);
+    }
+
+    /**
+     * Display a listing of the resource.
+     * @param \App\Models\API\V1\Post $post
+     * @return JsonResponse|mixed
+     */
+    public function indexByPost(Post $post): JsonResponse
+    {
+        $comments = $post->comments;
+
+        return response()
+            ->json(CommentResource::collection($comments), JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Display a listing of the resource.
+     * @param \App\Models\API\V1\Review $review
+     * @return JsonResponse|mixed
+     */
+    public function indexByReview(Review $review): JsonResponse
+    {
+        $comments = $review->comments;
+
+        return response()
+            ->json(CommentResource::collection($comments), JsonResponse::HTTP_OK);
     }
 
     /**
