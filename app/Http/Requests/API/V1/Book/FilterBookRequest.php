72a86db5b216aec0fb872fd6b717375c0d5bc83a
natanael <rojonatanael99@gmail.com>
2025-03-19 21:10:56 -0400

    feature(Creating FilterBookRequest): Adding a FilterBookRequest to manage validations related with filters

---
+++ app/Http/Requests/API/V1/Book/FilterBookRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Book;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class FilterBookRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['string', 'max:255'],
+            'author_name' => ['string', 'max:255'],
+            'genre_name' => ['string', 'max:255'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors()
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
