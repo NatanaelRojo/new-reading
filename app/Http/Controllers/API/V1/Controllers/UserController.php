6bfa415e8cd606c52274830c7d67e07a902c3c59
natanael <rojonatanael99@gmail.com>
2025-03-30 13:53:23 -0400

    feature(Separating toggleFollow into tuw functions)

--- app/Http/Controllers/API/V1/Controllers/UserController.php
+++ app/Http/Controllers/API/V1/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API\V1\Controllers;
 
+use App\Http\Requests\API\V1\User\StoreUserRequest;
 use App\Http\Requests\API\V1\User\UpdateUserRequest;
 use App\Http\Resources\API\V1\User\UserResource;
 use App\Models\User;
@@ -24,7 +25,7 @@ class UserController
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): JsonResponse
+    public function store(StoreUserRequest $request): JsonResponse
     {
         $newUser = User::query()
             ->create($request->validated());
@@ -61,24 +62,43 @@ class UserController
     }
 
     /**
-     * Toggle follow
+     * Follow a user if not already following.
      *
-     * @param Request $request
-     * @param User $user
-     * @return JsonResponse
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\User $user
+     * @return JsonResponse|mixed
      */
-    public function toggleFollow(Request $request, User $user): JsonResponse
+    public function follow(Request $request, User $user): JsonResponse
     {
-        $currentUser = $request->user();
+        $authUser = $request->user();
 
-        if ($user->isFollowing($user)) {
-            $currentUser->unfollow($user);
-
-            return response()->json(['message' => 'Unfollowed'], JsonResponse::HTTP_OK);
-        } else {
-            $currentUser->follow($user);
-
-            return response()->json(['message' => 'Followed'], JsonResponse::HTTP_OK);
+        if (!$user->isFollowing($authUser)) {
+            $authUser->follow($user);
         }
+
+        return response()->json([
+            'message' => "You are now following {$user->name}."
+        ], JsonResponse::HTTP_OK);
     }
+
+    /**
+     * Unfollow a user if already following.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\User $user
+     * @return JsonResponse|mixed
+     */
+    public function unfollow(Request $request, User $user): JsonResponse
+    {
+        $authUser = $request->user();
+
+        if ($authUser->isFollowing($user)) {
+            $authUser->unfollow($user);
+        }
+
+        return response()->json([
+            'message' => "You have unfollowed {$user->name}."
+        ], JsonResponse::HTTP_OK);
+    }
+
 }
