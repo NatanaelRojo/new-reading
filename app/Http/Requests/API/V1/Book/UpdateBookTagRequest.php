5ece39165daab7400a9f4715b928530b6a112f31
natanael <rojonatanael99@gmail.com>
2025-04-05 16:07:08 -0400

    feature(UpdateBookTagRequest): Creating a form request for validating related to change a tag of a book

---
+++ app/Http/Requests/API/V1/Book/UpdateBookTagRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Book;
+
+use App\Models\API\V1\Tag;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class UpdateBookTagRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tag_id' => ['required', 'integer', 'exists:' . Tag::class . ',id'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors()
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
