9e4014a42ab06d0cac766e0d83ce1ae4597f79d9
natanael <rojonatanael99@gmail.com>
2025-04-05 16:05:04 -0400

    feature(UpdateBookReadingProgressRequest): Creating a form request for validating related to update the reading progress of a book

---
+++ app/Http/Requests/API/V1/Book/UpdateBookReadingProgressRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Book;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class UpdateBookReadingProgressRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'pages_read' => [
+                'required',
+                'integer',
+                'min:1',
+            ],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors()
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
