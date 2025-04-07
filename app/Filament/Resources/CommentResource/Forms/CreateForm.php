2d530114afbaee59c6efabc026179346c9b7cfc4
natanael <rojonatanael99@gmail.com>
2025-04-06 21:35:38 -0400

    fix(MorphToSelect): Implementing a MorphToSelect due to morphologic relationship

--- app/Filament/Resources/CommentResource/Forms/CreateForm.php
+++ app/Filament/Resources/CommentResource/Forms/CreateForm.php
@@ -4,6 +4,9 @@ namespace App\Filament\Resources\CommentResource\Forms;
 
 use App\Filament\Resources\BookResource\Forms\CreateForm as FormsCreateForm;
 use App\Filament\Resources\UserResource\Forms\CreateForm as UserResourceFormsCreateForm;
+use App\Models\API\V1\Book;
+use Filament\Forms\Components\MorphToSelect;
+use Filament\Forms\Components\MorphToSelect\Type;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 
@@ -12,15 +15,21 @@ class CreateForm
     public static function make(): array
     {
         return [
-            Select::make('book_id')
-            ->required()
-                ->relationship(
-                    name: 'book',
-                    titleAttribute: 'title'
-                )
-                ->preload()
-                ->searchable()
-                ->createOptionForm(FormsCreateForm::getFields()),
+            MorphToSelect::make('commentable')
+                ->types([
+                    Type::make(Book::class)
+                        ->titleAttribute('title'),
+                ])->preload()
+                ->searchable(),
+            // Select::make('book_id')
+            // ->required()
+            //     ->relationship(
+            //         name: 'book',
+            //         titleAttribute: 'title'
+            //     )
+            //     ->preload()
+            //     ->searchable(),
+            //     // ->createOptionForm(FormsCreateForm::getFields()),
             Select::make('user_id')
                 ->required()
                 ->relationship(
