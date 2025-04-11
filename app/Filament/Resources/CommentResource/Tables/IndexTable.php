18e6e12246a883934e920aa91c0b4cebc5de190b
natanael <rojonatanael99@gmail.com>
2025-04-11 09:36:03 -0400

    feature(Filters): Implementing a filter for Comments with the related models

--- app/Filament/Resources/CommentResource/Tables/IndexTable.php
+++ app/Filament/Resources/CommentResource/Tables/IndexTable.php
@@ -2,30 +2,37 @@
 
 namespace App\Filament\Resources\CommentResource\Tables;
 
+use App\Models\API\V1\Book;
+use App\Models\API\V1\Post;
 use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 
 class IndexTable
 {
+    public const array COMMENTABLE_OPTIONS = [
+        Book::class => 'Books',
+        Post::class => 'Posts',
+    ];
+
     public static function getColumns(): array
     {
         return [
-            TextColumn::make('user.name')
-                ->searchable(),
-            TextColumn::make('commentable.title'),
-            TextColumn::make('body')
-                ->limit(50),
+            TextColumn::make('user.name')->searchable(),
+            TextColumn::make('related_to'),
+            TextColumn::make('body')->limit(50),
         ];
     }
 
     public static function getFilters(): array
     {
         return [
-            //
+            SelectFilter::make('commentable_type')->label('Type')
+                ->options(self::COMMENTABLE_OPTIONS),
         ];
     }
 
