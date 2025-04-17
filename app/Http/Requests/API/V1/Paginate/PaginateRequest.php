0669c963cfda5ecc0cb0e0373b7ae313e9c314dc
natanael <rojonatanael99@gmail.com>
2025-04-16 23:45:34 -0400

    feature(Paginate form request): Implementing a basic pagination form request

---
+++ app/Http/Requests/API/V1/Paginate/PaginateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Paginate;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class PaginateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'page' => ['sometimes', 'integer', 'min:1'],
+            'per_page' => ['sometimes', 'integer', 'min:1', 'max:100'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => $validator->errors(),
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
