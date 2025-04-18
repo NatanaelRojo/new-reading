356924679ce59ec9720163ad212f87f2fbec024b
natanael <rojonatanael99@gmail.com>
2025-04-18 16:28:33 -0400

    test(Documenting and pagination): Applying pagination to tests and documenting Comment test functions

--- tests/Feature/API/V1/CommentApiTest.php
+++ tests/Feature/API/V1/CommentApiTest.php
@@ -41,33 +41,39 @@ class CommentApiTest extends TestCase
         Sanctum::actingAs($this->user);
     }
 
+    /**
+     * This PHP function tests for an empty comment list and asserts that the response is OK and the JSON
+     * count is 0.
+     */
     public function test_get_empty_comment_list(): void
     {
-        $response = $this->getJson(route('comments.index'));
-
-        $response->assertOk();
-        $response->assertJsonCount(0);
+        $this->getJson(route('comments.index'))
+        ->assertOk()
+        ->assertJsonCount(0, 'data');
     }
 
+    /**
+     * The function tests creating a comment for a post by following the post's user and sending a JSON
+     * request with a body to the appropriate route.
+     */
     public function test_create_comment_for_a_post(): void
     {
         $post = Post::inRandomOrder()->first();
         $this->user->follow($post->user);
 
-        $response = $this->postJson(
+        $this->postJson(
             route('posts.comments.store', $post->slug),
             [
                 'body' => fake()->sentence(),
             ]
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_CREATED)
-            ->assertJsonCount(1);
-
-        $response = $this->getJson(route('posts.comments.index', $post->slug));
-        $response->assertJsonCount(1);
+        )->assertStatus(JsonResponse::HTTP_CREATED)
+        ->assertJsonCount(1);
     }
 
+    /**
+     * The function `test_create_several_comments_for_a_post` creates multiple comments for a post and
+     * asserts the successful creation of each comment.
+     */
     public function test_create_several_comments_for_a_post(): void
     {
         $post = Post::inRandomOrder()->first();
@@ -76,45 +82,49 @@ class CommentApiTest extends TestCase
         for ($i = 0; $i < 5; $i++) {
             $currentCommentBody = fake()->sentence();
 
-            $response = $this->postJson(
+            $this->postJson(
                 route('posts.comments.store', $post->slug),
                 [
                     'body' => $currentCommentBody,
                 ]
-            );
-
-            $response->assertStatus(JsonResponse::HTTP_CREATED)
-                ->assertJsonCount(1)
-                ->assertJsonFragment([
-                    'body' => $currentCommentBody,
-                ]);
+            )->assertStatus(JsonResponse::HTTP_CREATED)
+            ->assertJsonCount(1)
+            ->assertJsonFragment([
+                'body' => $currentCommentBody,
+            ]);
         }
     }
 
+    /**
+     * The function `test_create_a_comment_for_a_review` tests creating a comment for a review in PHP
+     * using Laravel testing.
+     */
     public function test_create_a_comment_for_a_review(): void
     {
         $review = Review::inRandomOrder()->first();
         $this->user->follow($review->user);
         $reviewCommentBody = fake()->sentence();
 
-        $response = $this->postJson(
+        $this->postJson(
             route('reviews.comments.store', $review->id),
             [
                 'body' => $reviewCommentBody,
             ]
-        );
+        )->assertStatus(JsonResponse::HTTP_CREATED)
+        ->assertJsonCount(1)
+        ->assertJsonFragment([
+            'body' => $reviewCommentBody,
+        ]);
 
-        $response->assertStatus(JsonResponse::HTTP_CREATED)
-            ->assertJsonCount(1)
-            ->assertJsonFragment([
-                'body' => $reviewCommentBody,
-            ]);
-
-        $response = $this->getJson(route('reviews.comments.index', $review->id));
-        $response->assertStatus(JsonResponse::HTTP_OK)
-            ->assertJsonCount(1);
+        $this->getJson(route('reviews.comments.index', $review->id))
+        ->assertStatus(JsonResponse::HTTP_OK)
+        ->assertJsonCount(1, 'data');
     }
 
+    /**
+     * The function `test_create_several_comments_for_a_review` creates multiple comments for a review
+     * and asserts the successful creation of each comment.
+     */
     public function test_create_several_comments_for_a_review(): void
     {
         $review = Review::inRandomOrder()->first();
@@ -123,14 +133,12 @@ class CommentApiTest extends TestCase
         for ($i = 0; $i < 5; $i++) {
             $currentCommentBody = fake()->sentence();
 
-            $response = $this->postJson(
+            $this->postJson(
                 route('reviews.comments.store', $review->id),
                 [
                     'body' => $currentCommentBody,
                 ]
-            );
-
-            $response->assertStatus(JsonResponse::HTTP_CREATED)
+            )->assertStatus(JsonResponse::HTTP_CREATED)
                 ->assertJsonCount(1)
                 ->assertJsonFragment([
                     'body' => $currentCommentBody,
@@ -138,6 +146,9 @@ class CommentApiTest extends TestCase
         }
     }
 
+    /**
+     * This function tests the functionality to show all comments for a specific post.
+     */
     public function test_show_all_comments_for_a_post(): void
     {
         $post = Post::inRandomOrder()->first();
@@ -152,12 +163,15 @@ class CommentApiTest extends TestCase
             );
         }
 
-        $response = $this->getJson(route('posts.comments.index', $post->slug));
-
-        $response->assertStatus(JsonResponse::HTTP_OK)
-            ->assertJsonCount(5);
+        $this->getJson(route('posts.comments.index', $post->slug))
+        ->assertStatus(JsonResponse::HTTP_OK)
+        ->assertJsonCount(5, 'data');
     }
 
+    /**
+     * This function tests showing all comments for a review by creating 5 comments and then retrieving
+     * them.
+     */
     public function test_show_all_comments_for_a_review(): void
     {
         $review = Review::inRandomOrder()->first();
@@ -172,53 +186,57 @@ class CommentApiTest extends TestCase
             );
         }
 
-        $response = $this->getJson(route('reviews.comments.index', $review->id));
-
-        $response->assertStatus(JsonResponse::HTTP_OK)
-            ->assertJsonCount(5);
+        $this->getJson(route('reviews.comments.index', $review->id))
+        ->assertStatus(JsonResponse::HTTP_OK)
+        ->assertJsonCount(5, 'data');
     }
 
+    /**
+     * The function tests showing a comment for a user.
+     */
     public function test_show_a_comment_for_a_user(): void
     {
         $comment = Comment::factory()->create();
 
-        $response = $this->getJson(
+        $this->getJson(
             route('comments.show', $comment->slug)
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_OK)
-            ->assertJsonCount(1);
+        )->assertStatus(JsonResponse::HTTP_OK)
+        ->assertJsonCount(1);
     }
 
+    /**
+     * The function `test_update_a_comment` tests updating a comment's body and asserts the response
+     * status, JSON count, and JSON fragment.
+     */
     public function test_update_a_comment(): void
     {
         $comment = Comment::factory()->create();
 
         $newCommentBody = fake()->sentence();
 
-        $response = $this->putJson(
+        $this->putJson(
             route('comments.update', $comment->slug),
             [
                 'body' => $newCommentBody,
             ]
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_OK)
+        )->assertStatus(JsonResponse::HTTP_OK)
             ->assertJsonCount(1)
             ->assertJsonFragment([
                 'body' => $newCommentBody,
             ]);
     }
 
+    /**
+     * The function `test_delete_a_comment` tests the deletion of a comment in a PHP application using
+     * Laravel.
+     */
     public function test_delete_a_comment(): void
     {
         $comment = Comment::factory()->create();
 
-        $response = $this->deleteJson(
+        $this->deleteJson(
             route('comments.destroy', $comment->slug)
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_NO_CONTENT);
+        )->assertStatus(JsonResponse::HTTP_NO_CONTENT);
 
         $this->assertDatabaseMissing('comments', ['id' => $comment->id]);
     }
