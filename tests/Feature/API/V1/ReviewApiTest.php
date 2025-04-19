d21891bfbb6fc7aa28de397c10dcca95485eb736
natanael <rojonatanael99@gmail.com>
2025-04-19 14:54:05 -0400

    test(Pagination & documentation): Implementing pagination for Review test functions and documenting them

--- tests/Feature/API/V1/ReviewApiTest.php
+++ tests/Feature/API/V1/ReviewApiTest.php
@@ -19,6 +19,9 @@ class ReviewApiTest extends TestCase
     private User $user;
     private int $reviewsAmount = 5;
 
+    /**
+     * The setUp function initializes data for testing in a PHP unit test class.
+     */
     protected function setUp(): void
     {
         parent::setUp();
@@ -33,26 +36,32 @@ class ReviewApiTest extends TestCase
         Sanctum::actingAs($this->user);
     }
 
+    /**
+     * The function `test_create_a_review` tests the creation of a review for a book in a PHP
+     * application.
+     */
     public function test_create_a_review(): void
     {
         $book = Book::inRandomOrder()->first();
 
-        $response = $this->postJson(route('reviews.store'), [
+        $this->postJson(route('reviews.store'), [
             'book_id' => $book->id,
             'rating' => fake()->numberBetween(1, 5),
             'comment' => fake()->text(),
+        ])->assertStatus(JsonResponse::HTTP_CREATED)
+        ->assertJsonStructure([
+            'comment',
+            'rating',
         ]);
 
-        $response->assertStatus(JsonResponse::HTTP_CREATED)
-            ->assertJsonStructure([
-                'comment',
-                'rating',
-            ]);
-
-        $response = $this->getJson(route('reviews.index'));
-        $response->assertJsonCount(Review::count());
+        $this->getJson(route('reviews.index'))
+            ->assertJsonCount(10, 'data');
     }
 
+    /**
+     * The function creates a review for a user for a specific book, including setting completion
+     * status and verifying the review creation.
+     */
     public function test_create_a_review_for_a_user(): void
     {
         $book = Book::inRandomOrder()->first();
@@ -64,32 +73,42 @@ class ReviewApiTest extends TestCase
         $pivot->save();
         $book->completeBook($this->user);
 
-        $response = $this->postJson(
+        $this->postJson(
             route('books.reviews.store', $book->slug),
             [
             'rating' => fake()->numberBetween(1, 5),
             'comment' => fake()->text(),
         ]
-        );
+        )->assertStatus(JsonResponse::HTTP_CREATED)
+        ->assertJsonStructure([
+            'comment',
+            'rating',
+        ]);
 
-        $response->assertStatus(JsonResponse::HTTP_CREATED)
-            ->assertJsonStructure([
-                'comment',
-                'rating',
-            ]);
-
-        $response = $this->getJson(route('books.reviews.index', $book->slug));
-        $response->assertJsonCount($book->reviews()->count());
+        $this->getJson(route('books.reviews.index', $book->slug))
+            ->assertJsonCount(
+                $book->reviews()->count() <= 10
+                    ? $book->reviews()->count()
+                    : 10,
+                'data'
+            );
     }
 
+    /**
+     * This function tests the retrieval of a list of reviews and asserts that the response status is
+     * OK and the JSON count matches the total number of reviews.
+     */
     public function test_get_a_list_of_reviews(): void
     {
         Review::factory()->count($this->reviewsAmount)->create();
 
         $this->getJson(route('reviews.index'))->assertStatus(JsonResponse::HTTP_OK)
-        ->assertJsonCount(Review::count());
+        ->assertJsonCount(10, 'data');
     }
 
+    /**
+     * The function tests getting a list of reviews for a user.
+     */
     public function test_get_a_list_of_reviews_for_a_user(): void
     {
         $book = Book::factory()->create();
@@ -102,76 +121,89 @@ class ReviewApiTest extends TestCase
 
         $response = $this->getJson(
             route('users.reviews.index', $this->user->id)
-        );
+        )->assertStatus(JsonResponse::HTTP_OK)
+        ->assertJsonCount(1, 'data');
 
-        $response->assertStatus(JsonResponse::HTTP_OK)
-            ->assertJsonCount(1);
+        foreach ($response->json()['data'] as $review) {
+            $this->assertArrayHasKey('comment', $review);
+            $this->assertArrayHasKey('rating', $review);
+        }
     }
 
+    /**
+     * The function `test_show_a_review` tests the endpoint for displaying a review and asserts the
+     * response structure.
+     */
     public function test_show_a_review(): void
     {
         $review = Review::factory()->create();
 
-        $response = $this->getJson(
+        $this->getJson(
             route('reviews.show', $review->id)
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_OK)
-            ->assertJsonStructure([
-                'comment',
-                'rating',
-            ]);
+        )->assertStatus(JsonResponse::HTTP_OK)
+        ->assertJsonStructure([
+            'comment',
+            'rating',
+        ]);
     }
 
+    /**
+     * The function `test_update_a_review` tests updating a review in a Laravel application using
+     * factory-generated data.
+     */
     public function test_update_a_review(): void
     {
         $review = Review::factory()->create();
         $updatedReview = Review::factory()->make();
 
-        $response = $this->putJson(
+        $this->putJson(
             route('reviews.update', $review->id),
             $updatedReview->toArray()
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_OK)
-            ->assertJsonStructure([
-                'comment',
-                'rating',
-            ]);
+        )->assertStatus(JsonResponse::HTTP_OK)
+        ->assertJsonStructure([
+            'comment',
+            'rating',
+        ]);
     }
 
+    /**
+     * The function `test_delete_a_review` tests the deletion of a review using Laravel's testing
+     * framework.
+     */
     public function test_delete_a_review(): void
     {
         $review = Review::factory()->create();
 
         $response = $this->deleteJson(
             route('reviews.destroy', $review->id)
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_NO_CONTENT);
+        )->assertStatus(JsonResponse::HTTP_NO_CONTENT);
 
         $this->assertDatabaseMissing('reviews', ['id' => $review->id]);
     }
 
+    /**
+     * The function `test_like_a_review` tests the functionality of liking a review in a PHP
+     * environment using Laravel testing.
+     */
     public function test_like_a_review(): void
     {
         $review = Review::factory()->create();
 
-        $response = $this->postJson(
+        $this->postJson(
             route('reviews.like', $review->id)
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_OK);
+        )->assertStatus(JsonResponse::HTTP_OK);
     }
 
+    /**
+     * The function `test_dislike_a_review` tests the functionality of disliking a review in a PHP
+     * application using Laravel.
+     */
     public function test_dislike_a_review(): void
     {
         $review = Review::factory()->create();
 
-        $response = $this->postJson(
+        $this->postJson(
             route('reviews.dislike', $review->id)
-        );
-
-        $response->assertStatus(JsonResponse::HTTP_OK);
+        )->assertStatus(JsonResponse::HTTP_OK);
     }
 }
