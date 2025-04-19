299bc02d5d7d493155db476aa61773f37d98f41e
natanael <rojonatanael99@gmail.com>
2025-04-19 15:31:47 -0400

    feature(Pagination for Tags)

--- app/Http/Controllers/API/V1/TagController.php
+++ app/Http/Controllers/API/V1/TagController.php
@@ -2,26 +2,26 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Http\Requests\API\V1\Paginate\PaginateRequest;
 use App\Http\Requests\API\V1\Tag\StoreTagRequest;
 use App\Http\Requests\API\V1\Tag\UpdateTagRequest;
 use App\Http\Resources\API\V1\Tag\TagResource;
 use App\Models\API\V1\Tag;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class TagController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(PaginateRequest $request): AnonymousResourceCollection
     {
-        $tags = Tag::all();
+        $tags = Tag::query()
+            ->paginate($request->query('per_page', 10));
 
-        return response()->json(
-            TagResource::collection($tags),
-            JsonResponse::HTTP_OK
-        );
+        return TagResource::collection($tags);
     }
 
     /**
