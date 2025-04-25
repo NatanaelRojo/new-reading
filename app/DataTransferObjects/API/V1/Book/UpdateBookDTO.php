a2d3dafb1a84d0fd7236fe519a36b964daf4bf29
natanael <rojonatanael99@gmail.com>
2025-04-25 18:43:16 -0400

    feature(UpdateBookDTO): Implementing a basic UpdateBookDTO

---
+++ app/DataTransferObjects/API/V1/Book/UpdateBookDTO.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataTransferObjects\API\V1\Book;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+/**
+ * Data Transfer Object for UpdateBook
+ *
+ * Auto-generated from App\Http\Requests\API\V1\Book\UpdateBookRequest.
+ */
+class UpdateBookDTO extends DataTransferObject
+{
+    public function __construct(
+public readonly ?string $title = null,
+public readonly ?string $synopsis = null,
+public readonly ?string $isbn = null,
+public readonly \Carbon\Carbon|string|null $published_at = null,
+public readonly ?int $pages_amount = null,
+public readonly ?int $chapters_amount = null,
+    ) {}
+}
