8786e93c044ce8f055d7758fc74fe400a89b174a
natanael <rojonatanael99@gmail.com>
2025-04-25 20:56:30 -0400

    feature(StoreCommentDTO): Implementing a basic StoreCommentDTO

---
+++ app/DataTransferObjects/API/V1/Comment/StoreCommentDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataTransferObjects\API\V1\Comment;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+/**
+ * Data Transfer Object for StoreComment
+ *
+ * Auto-generated from App\Http\Requests\API\V1\Comment\StoreCommentRequest.
+ */
+class StoreCommentDTO extends DataTransferObject
+{
+    public function __construct(
+        public readonly string $body,
+        public readonly int $user_id,
+        public readonly int $commentable_id,
+        public readonly string $commentable_type,
+    ) {
+    }
+}
