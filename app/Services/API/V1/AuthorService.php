a2b98e8ad91469851b4d3995a9d317fd93cebd38
natanael <rojonatanael99@gmail.com>
2025-04-27 14:18:36 -0400

    feature(AuthorService): Implementing a basic AuthorService

---
+++ app/Services/API/V1/AuthorService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services\API\V1;
+
+use App\DataTransferObjects\API\V1\Author\StoreAuthorDTO;
+use App\DataTransferObjects\API\V1\Author\UpdateAuthorDTO;
+use App\Models\API\V1\Author;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Service for AuthorService
+ */
+class AuthorService
+{
+    /**
+     * Get all instances from the database
+     *
+     * @return LengthAwarePaginator
+     */
+    public function index(int $per_page = 10): LengthAwarePaginator
+    {
+        return Author::with('books')
+        ->paginate($per_page);
+    }
+
+    /**
+     * Store a new instance in the database
+     * @param StoreAuthorDTO $storeAuthorDto
+     *
+     * @return Author
+     */
+    public function store(StoreAuthorDTO $storeAuthorDto): Author
+    {
+        return Author::query()
+        ->create($storeAuthorDto->toArray());
+    }
+
+    /**
+     * Get a single instance from the database
+     */
+    public function show()
+    {
+        //
+    }
+
+    /**
+     * Update an existing instance in the database
+     */
+    public function update(UpdateAuthorDTO $updateAuthorDto, Author $author): void
+    {
+        $author->update($updateAuthorDto->toArray());
+    }
+
+    /**
+     * Delete an existing instance from the database
+     */
+    public function destroy(Author $author): void
+    {
+        $author->delete();
+    }
+}
