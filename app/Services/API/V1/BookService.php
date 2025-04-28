042a7c77e65dfaf444cdabe2066e88ada5463b0b
natanael <rojonatanael99@gmail.com>
2025-04-27 21:35:56 -0400

    feature(Update reading progress of a book): Implementing methods to update the reading progress of a book

--- app/Services/API/V1/BookService.php
+++ app/Services/API/V1/BookService.php
@@ -5,7 +5,10 @@ namespace App\Services\API\V1;
 use App\DataTransferObjects\API\V1\Book\FilterBookDTO;
 use App\DataTransferObjects\API\V1\Book\StoreBookDTO;
 use App\DataTransferObjects\API\V1\Book\UpdateBookDTO;
+use App\DataTransferObjects\API\V1\Book\UpdateBookReadingProgressDTO;
 use App\Models\API\V1\Book;
+use App\Models\API\V1\Tag;
+use App\Models\User;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 /**
@@ -57,5 +60,35 @@ class BookService
     public function destroy(Book $book): void
     {
         $book->delete();
+    }
+
+    /**
+     * Update the progress of a user for a specific book.
+     *
+     * @param \App\DataTransferObjects\API\V1\Book\UpdateBookReadingProgressDTO $updateBookReadingProgressDto
+     * @return void
+     */
+    public function updateUserProgress(UpdateBookReadingProgressDTO $updateBookReadingProgressDto): void
+    {
+        if (!is_null($updateBookReadingProgressDto->user) || !is_null($updateBookReadingProgressDto->pagesRead)) {
+            $updateBookReadingProgressDto->pagesRead == $updateBookReadingProgressDto->book->pages_amount ? $this->completeBook($updateBookReadingProgressDto->book, $updateBookReadingProgressDto->user) : $updateBookReadingProgressDto->book->readingProgress = $updateBookReadingProgressDto->pagesRead;
+        }
+    }
+
+    /**
+     * Complete a book for a specific user.
+     *
+     * @param \App\Models\API\V1\Book $book
+     * @param \App\Models\User $user
+     * @return void
+     */
+    private function completeBook(Book $book, User $user): void
+    {
+        $tag = Tag::query()
+            ->firstWhere('name', config('tags.default_tags')[2]);
+        $book->users()->updateExistingPivot($user->id, [
+            'tag_id' => $tag->id,
+            'pages_read' => $book->pages_amount,
+        ]);
     }
 }
