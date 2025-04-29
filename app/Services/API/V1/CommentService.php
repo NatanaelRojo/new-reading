8aa5444ca957016cfb869bbf44404785e4e75fdb
natanael <rojonatanael99@gmail.com>
2025-04-29 13:28:54 -0400

    feature(storeByPost): Implementing the function storeByPost in CommentService

---
+++ app/Services/API/V1/CommentService.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Services\API\V1;
+
+use App\DataTransferObjects\API\V1\Comment\StoreCommentByPostDTO;
+use App\DataTransferObjects\API\V1\Comment\StoreCommentDTO;
+use App\DataTransferObjects\API\V1\Comment\UpdateCommentDTO;
+use App\DataTransferObjects\API\V1\Paginate\PaginateDTO;
+use App\Exceptions\API\V1\User\UserNotFollowingException;
+use App\Models\API\V1\Comment;
+use App\Models\API\V1\Post;
+use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Service for CommentService
+ */
+class CommentService
+{
+    /**
+     * Get all instances from the database
+     */
+    public function index(PaginateDTO $paginateDto): LengthAwarePaginator
+    {
+        return Comment::query()
+        ->paginate($paginateDto->perPage ?? 10);
+    }
+
+    /**
+     * Store a new instance in the database
+     */
+    public function store(StoreCommentDTO $storeCommentDto): Comment
+    {
+        return Comment::query()
+            ->create($storeCommentDto->toArray());
+    }
+
+    /**
+     * Store a new instance in the database
+     *
+     * @param \App\DataTransferObjects\API\V1\Comment\StoreCommentByPostDTO $storeCommentByPostDto
+     * @throws \App\Exceptions\API\V1\User\UserNotFollowingException
+     * @return Comment
+     */
+    public function storeByPost(StoreCommentByPostDTO $storeCommentByPostDto): Comment
+    {
+        if (!$storeCommentByPostDto->user->isFollowing($storeCommentByPostDto->post->user)) {
+            throw new UserNotFollowingException();
+        }
+
+        return $storeCommentByPostDto->post->comments()
+            ->create($storeCommentByPostDto->toArray());
+    }
+
+    /**
+     * Get a single instance from the database
+     */
+    public function show()
+    {
+        //
+    }
+
+    /**
+     * Update an existing instance in the database
+     */
+    public function update(UpdateCommentDTO $updateCommentDto, Comment $comment): void
+    {
+        $comment->update($updateCommentDto->toArray());
+    }
+
+    /**
+     * Delete an existing instance from the database
+     */
+    public function destroy(Comment $comment): void
+    {
+        $comment->delete();
+    }
+}
