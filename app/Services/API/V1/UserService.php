5680b2f72c6af5de07de2578ae28c7186ece8989
natanael <rojonatanael99@gmail.com>
2025-05-01 13:52:19 -0400

    feature(UserService): Implementing a basic UserService

---
+++ app/Services/API/V1/UserService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services\API\V1;
+
+use App\DataTransferObjects\API\V1\Paginate\PaginateDTO;
+use App\DataTransferObjects\API\V1\User\StoreUserDTO;
+use App\DataTransferObjects\API\V1\User\UpdateUserDTO;
+use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Service for UserService
+ */
+class UserService
+{
+    /**
+     * Get all instances from the database
+     */
+    public function index(PaginateDTO $paginateDto): LengthAwarePaginator
+    {
+        return User::with(['following', 'followers'])
+        ->paginate($paginateDto->perPage ?? 10);
+    }
+
+    /**
+     * Store a new instance in the database
+     */
+    public function store(StoreUserDTO $storeUserDto): User
+    {
+        return User::query()
+        ->create($storeUserDto->toArray());
+    }
+
+    /**
+     * Get a single instance from the database
+     */
+    public function show()
+    {
+        //
+    }
+
+    /**
+     * Update an existing instance in the database
+     */
+    public function update(UpdateUserDTO $updateUserDto, User $user): void
+    {
+        $user->update($updateUserDto->toArray());
+    }
+
+    /**
+     * Delete an existing instance from the database
+     */
+    public function destroy(User $user): void
+    {
+        $user->delete();
+    }
+}
