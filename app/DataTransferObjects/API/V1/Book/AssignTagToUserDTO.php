ad9ad1bea230b3265c8e9b30ff2df74324e575c7
natanael <rojonatanael99@gmail.com>
2025-05-01 21:06:05 -0400

    feature(AssignTagToUserDTO): Implementing a basic AssignTagToUserDTO

---
+++ app/DataTransferObjects/API/V1/Book/AssignTagToUserDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataTransferObjects\API\V1\Book;
+
+use App\DataTransferObjects\Base\BaseApiDTO;
+use App\Models\API\V1\Book;
+use App\Models\API\V1\Tag;
+use App\Models\User;
+use Spatie\DataTransferObject\DataTransferObject;
+
+/**
+ * Data Transfer Object for AssignTagToUser
+ *
+ * Auto-generated from .
+ */
+class AssignTagToUserDTO extends BaseApiDTO
+{
+    public function __construct(
+        public readonly Book $book,
+        public readonly User $user,
+        public readonly Tag $tag,
+    ) {
+    }
+}
