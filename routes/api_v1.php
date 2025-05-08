0db9645eb8dd7792a13ae87852000f25e1f8a29c
natanael <rojonatanael99@gmail.com>
2025-05-07 21:51:38 -0400

    fix(api_v1): Refactoring routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,85 +1,71 @@
 <?php
 
-use App\Http\Controllers\API\V1\CommentController;
-use App\Http\Controllers\API\V1\AuthController;
-use App\Http\Controllers\API\V1\AuthorController;
-use App\Http\Controllers\API\V1\BookController;
-use App\Http\Controllers\API\V1\GenreController;
-use App\Http\Controllers\API\V1\ReviewController;
-use App\Http\Controllers\API\V1\UserController;
-use App\Http\Controllers\API\V1\PostController;
-use App\Http\Controllers\API\V1\TagController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\{
+    AuthController,
+    AuthorController,
+    BookController,
+    CommentController,
+    GenreController,
+    PostController,
+    ReviewController,
+    TagController,
+    UserController
+};
 
-Route::get('/user', function (Request $request) {
-    return $request->user()->books;
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', fn ($request) => $request->user()->books);
 
-// Auth Routes
+    // RESTful resources
+    Route::apiResources([
+        'authors' => AuthorController::class,
+        'books' => BookController::class,
+        'comments' => CommentController::class,
+        'genres' => GenreController::class,
+        'posts' => PostController::class,
+        'users' => UserController::class,
+        'tags' => TagController::class,
+        'reviews' => ReviewController::class,
+    ]);
+
+    // Book nested routes
+    Route::prefix('books/{book}')->name('books.')->group(function () {
+        Route::post('comments', [CommentController::class, 'storeByBook'])->name('comments.store');
+        Route::get('posts', [PostController::class, 'indexByBook'])->name('posts.index');
+        Route::post('posts', [PostController::class, 'storeByBook'])->name('posts.store');
+        Route::put('tags', [BookController::class, 'updateTag'])->name('tags.update');
+        Route::put('reading-progress', [BookController::class, 'updateReadingProgress'])->name('reading-progress.update');
+        Route::get('reviews', [ReviewController::class, 'indexByBook'])->name('reviews.index');
+        Route::post('reviews', [ReviewController::class, 'storeByBook'])->name('reviews.store');
+    });
+
+    // Post nested routes
+    Route::prefix('posts/{post}')->name('posts.')->group(function () {
+        Route::get('comments', [CommentController::class, 'indexByPost'])->name('comments.index');
+        Route::post('comments', [CommentController::class, 'storeByPost'])->name('comments.store');
+    });
+
+    // Review nested routes
+    Route::prefix('reviews/{review}')->name('reviews.')->group(function () {
+        Route::get('comments', [CommentController::class, 'indexByReview'])->name('comments.index');
+        Route::post('comments', [CommentController::class, 'storeByReview'])->name('comments.store');
+        Route::post('like', [ReviewController::class, 'like'])->name('like');
+        Route::post('dislike', [ReviewController::class, 'dislike'])->name('dislike');
+    });
+
+    // User nested routes
+    Route::prefix('users/{user}')->name('users.')->group(function () {
+        Route::get('reviews', [ReviewController::class, 'indexByUser'])->name('reviews.index');
+        Route::get('posts', [PostController::class, 'indexByUser'])->name('posts.index');
+        Route::post('posts', [PostController::class, 'storeByUser'])->name('posts.store');
+        Route::post('follow', [UserController::class, 'follow'])->name('follow');
+        Route::delete('follow', [UserController::class, 'unfollow'])->name('unfollow');
+    });
+
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Auth (no middleware)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')
-    ->group(function () {
-        Route::apiResources([
-            'authors' => AuthorController::class,
-            'books' => BookController::class,
-            'comments' => CommentController::class,
-            'genres' => GenreController::class,
-            'posts' => PostController::class,
-            'users' => UserController::class,
-            'tags' => TagController::class,
-            'reviews' => ReviewController::class
-        ]);
-        Route::prefix('books/{book}')->group(function () {
-            Route::post('comments', [CommentController::class, 'storeByBook'])
-                ->name('books.comments.store');
-            Route::get('posts', [PostController::class, 'indexByBook'])
-            ->name('books.posts.index');
-            Route::post('posts', [PostController::class, 'storeByBook'])
-            ->name('books.posts.store');
-            Route::put('tags', [BookController::class, 'updateTag'])
-                ->name('books.tags.update');
-            Route::put('reading-progress', [BookController::class, 'updateReadingProgress'])
-            ->name('books.reading-progress.update');
-            Route::get('reviews', [ReviewController::class, 'indexByBook'])
-                ->name('books.reviews.index');
-            Route::post('reviews', [ReviewController::class, 'storeByBook'])
-                ->name('books.reviews.store');
-        });
-        Route::prefix('posts/{post}')->group(function () {
-            Route::get('comments', [CommentController::class, 'indexByPost'])
-                ->name('posts.comments.index');
-            Route::post('comments', [CommentController::class, 'storeByPost'])
-                ->name('posts.comments.store');
-        });
-        Route::prefix('reviews/{review}')->group(function () {
-            Route::get('comments', [CommentController::class, 'indexByReview'])
-                ->name('reviews.comments.index');
-            Route::post('comments', [CommentController::class, 'storeByReview'])
-            ->name('reviews.comments.store');
-            Route::post('like', [ReviewController::class, 'like'])
-                ->name('reviews.like');
-            Route::post('dislike', [ReviewController::class, 'dislike'])
-                ->name('reviews.dislike');
-        });
-        Route::prefix('users/{user}')->group(function () {
-            Route::get('reviews', [ReviewController::class, 'indexByUser'])
-            ->name('users.reviews.index');
-            Route::get('posts', [PostController::class, 'indexByUser'])
-                ->name('users.posts.index');
-            Route::post('posts', [PostController::class, 'storeByUser'])
-                ->name('users.posts.store');
-        });
-        Route::post('/logout', [AuthController::class, 'logout']);
-    });
-
-Route::middleware('auth:sanctum')
-    ->group(function () {
-        Route::controller(UserController::class)->group(function () {
-            Route::post('users/{user}/follow', 'follow')
-                ->name('users.follow');
-            Route::delete('users/{user}/follow', 'unfollow')
-                ->name('users.unfollow');
-        });
-    });
