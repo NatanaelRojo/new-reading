1f3e58c202c12e3e6029f4ed7a42877ee0506208
natanael <rojonatanael99@gmail.com>
2025-05-07 20:20:17 -0400

    fix(UpdateBookTagDTO): Changing eloquent models with primitives types ids

--- app/DataTransferObjects/API/V1/Book/UpdateBookTagDTO.php
+++ app/DataTransferObjects/API/V1/Book/UpdateBookTagDTO.php
@@ -5,6 +5,7 @@ namespace App\DataTransferObjects\API\V1\Book;
 use App\DataTransferObjects\Base\BaseApiDTO;
 use App\Models\API\V1\Book;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Spatie\DataTransferObject\DataTransferObject;
 
 /**
@@ -15,9 +16,18 @@ use Spatie\DataTransferObject\DataTransferObject;
 class UpdateBookTagDTO extends BaseApiDTO
 {
     public function __construct(
-        public readonly Book $book,
-        public readonly User $user,
+        public readonly int $bookId,
+        public readonly int $userId,
         public readonly int $tagId,
     ) {
+    }
+
+    public static function fromRequest(Request $request): static
+    {
+        return new static(
+            bookId: $request->route('book')->id,
+            userId: $request->user()->id,
+            tagId: $request->input('tag_id'),
+        );
     }
 }
