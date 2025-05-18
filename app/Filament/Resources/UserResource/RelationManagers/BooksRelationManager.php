5fb3304b88ccc7204efadc3403a19e9c76a94bd2
natanael <rojonatanael99@gmail.com>
2025-05-18 12:31:53 -0400

    feat(BooksRelationManager): Adding a badge to Books tab on the edit User view

--- app/Filament/Resources/UserResource/RelationManagers/BooksRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/BooksRelationManager.php
@@ -10,11 +10,17 @@ use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class BooksRelationManager extends RelationManager
 {
     protected static string $relationship = 'books';
+
+    public static function getBadge(Model $ownerRecord, string $pageClass): ?string
+    {
+        return $ownerRecord->books()->count();
+    }
 
     public function form(Form $form): Form
     {
