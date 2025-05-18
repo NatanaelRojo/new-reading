29013f1d811802dca13a0865f55092d2e79665c4
natanael <rojonatanael99@gmail.com>
2025-05-18 16:13:53 -0400

    feat(BookRelationManagers): Adding a badge to relation manager tables on edit Book view

--- app/Filament/Resources/BookResource/RelationManagers/ReviewsRelationManager.php
+++ app/Filament/Resources/BookResource/RelationManagers/ReviewsRelationManager.php
@@ -10,11 +10,17 @@ use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ReviewsRelationManager extends RelationManager
 {
     protected static string $relationship = 'reviews';
+
+                public static function getBadge(Model $ownerRecord, string $pageClass): ?string
+    {
+        return $ownerRecord->reviews()->count();
+    }
 
     public function form(Form $form): Form
     {
