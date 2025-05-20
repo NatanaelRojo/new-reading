0e409b974f4f0eeae24193f1b145075de17c170e
natanael <rojonatanael99@gmail.com>
2025-05-19 21:57:46 -0400

    feat(GenreResource): Changing the default icon

--- app/Filament/Resources/GenreResource.php
+++ app/Filament/Resources/GenreResource.php
@@ -18,7 +18,7 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class GenreResource extends Resource
 {
     protected static ?string $model = Genre::class;
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-queue-list';
     protected static string $searchPlaceHolderMessage = 'Search by genre\'s name...';
 
     public static function form(Form $form): Form
