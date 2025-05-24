dab6a4f90d574c1d56791a12d5a72319f2f36d34
natanael <rojonatanael99@gmail.com>
2025-05-24 10:02:50 -0400

    feat(BookRelationManager: Implementing BookRelationManager and its table on the Author edit view

---
+++ app/Filament/Resources/AuthorResource/Tables/BookRelationManagerTable.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources\AuthorResource\Tables;
+
+use App\Filament\Resources\Abstract\AbstractTable;
+use App\Filament\Resources\BookResource;
+use App\Models\API\V1\Book;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Actions\AttachAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\DetachAction;
+use Filament\Tables\Actions\DetachBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+
+class BookRelationManagerTable extends AbstractTable
+{
+    public static function getColumns(): array
+    {
+        return [
+            TextColumn::make('title')
+                ->searchable(),
+            TextColumn::make('synopsis')
+                ->limit(30),
+            TextColumn::make('isbn'),
+            TextColumn::make('pages_amount'),
+            TextColumn::make('chapters_amount'),
+            TextColumn::make('published_at')
+                ->date()
+                ->searchable(),
+            TextColumn::make('average_rating'),
+        ];
+    }
+
+    public static function getFilters(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getHeaderActions(): array
+    {
+        return [
+            AttachAction::make()
+                ->preloadRecordSelect()
+                ->multiple(),
+            CreateAction::make(),
+        ];
+    }
+
+    public static function getActions(?RelationManager $relationManager = null): array
+    {
+        $isRelation = $relationManager instanceof RelationManager;
+
+        return [
+            DetachAction::make(),
+            ViewAction::make(),
+            EditAction::make()
+                ->url(
+                    fn (Book $record): ?string => $isRelation ?
+                    BookResource::getUrl('edit', ['record' => $record])
+                    : null
+                ),
+            DeleteAction::make(),
+        ];
+    }
+
+    public static function getBulkActions(): array
+    {
+        return [
+            BulkActionGroup::make([
+                DetachBulkAction::make(),
+                DeleteBulkAction::make(),
+            ])
+
+                ];
+    }
+}
