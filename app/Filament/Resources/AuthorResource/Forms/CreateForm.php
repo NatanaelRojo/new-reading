19a0d93798f25371344b7c260a92df39dd96dff2
natanael <rojonatanael99@gmail.com>
2025-06-11 14:59:16 -0400

    feat(CreateForm): implement tabs for author and user details in create form

--- app/Filament/Resources/AuthorResource/Forms/CreateForm.php
+++ app/Filament/Resources/AuthorResource/Forms/CreateForm.php
@@ -4,14 +4,36 @@ namespace App\Filament\Resources\AuthorResource\Forms;
 
 use App\Filament\Resources\Abstract\AbstractCreateForm;
 use App\Filament\Resources\BookResource\Forms\CreateForm as FormsCreateForm;
+use App\Filament\Resources\UserResource\Forms\CreateForm as UserResourceFormsCreateForm;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 
 class CreateForm extends AbstractCreateForm
 {
+    public static function make(): array
+    {
+        return [
+            Tabs::make('Tabs')
+                ->tabs(static::getTabs()),
+        ];
+    }
+
+    private static function getTabs(): array
+    {
+        return [
+            Tab::make('Author Details')
+                ->schema(static::getFields()),
+            Tab::make('User details')
+                ->schema(static::getUserFields()),
+        ];
+    }
+
     public static function getFields(): array
     {
         return [
@@ -29,28 +51,28 @@ class CreateForm extends AbstractCreateForm
         ];
     }
 
-    public static function make(): array
+    private static function getUserFields(): array
     {
-        return [
-            TextInput::make('first_name')
+        return  [
+            TextInput::make('user.first_name')
                 ->required(),
-            TextInput::make('last_name')
+            TextInput::make('user.last_name')
                 ->required(),
-            TextInput::make('nationality')
+                TextInput::make('user.email')
+                ->required()
+                ->email(),
+            TextInput::make('user.name')
                 ->required(),
-            Textarea::make('biography')
+            TextInput::make('user.description')
                 ->required(),
-                FileUpload::make('image')
-                ->avatar()
-                ->imageEditor(),
-            Select::make('books')
-                ->multiple()
-                ->relationship(
-                    name: 'books',
-                    titleAttribute: 'title'
-                )->preload()
-                ->searchable()
-                ->createOptionForm(FormsCreateForm::getFields()),
+            TextInput::make('user.password')
+                ->required(),
+            DatePicker::make('user.birth_date')
+                ->required(),
+    //             Select::make('user.roles')
+    // ->multiple()
+    // ->relationship('roles', 'name')
+    // ->preload(),
         ];
     }
 }
