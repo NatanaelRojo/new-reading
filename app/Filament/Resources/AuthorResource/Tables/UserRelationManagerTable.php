b793196bbd26c13d2c0c9c455bce5a86afede64d
natanael <rojonatanael99@gmail.com>
2025-06-11 14:59:49 -0400

    feat(UserRelationManagerTable): add UserRelationManagerTable for managing user relationships in AuthorResource

---
+++ app/Filament/Resources/AuthorResource/Tables/UserRelationManagerTable.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources\AuthorResource\Tables;
+
+use App\Filament\Resources\Abstract\AbstractTable;
+use App\Filament\Resources\UserResource;
+use App\Models\API\V1\Author;
+use App\Models\User;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Actions\AssociateAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\DissociateAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Model;
+
+class UserRelationManagerTable extends AbstractTable
+{
+    public static function getColumns(): array
+    {
+        return [
+            TextColumn::make('name'),
+            TextColumn::make('email'),        ];
+    }
+
+    public static function getFilters(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getHeaderActions(): array
+    {
+        return [
+            AssociateAction::make()
+                ->preloadRecordSelect()
+                ->hidden(fn (?Model $record): bool => $record?->user()?->count() == 1),
+            CreateAction::make(),
+        ];
+    }
+
+    public static function getActions(?RelationManager $relationManager = null): array
+    {
+        $isRelation = $relationManager instanceof RelationManager;
+
+        return [
+            DissociateAction::make()
+                ->hidden(fn (?User $record): bool => $record?->author()?->count() == 1),
+                                    ViewAction::make(),
+            EditAction::make()
+                ->url(
+                    fn (User $record): ?string => $isRelation ?
+                    UserResource::getUrl('edit', ['record' => $record])
+                    : null
+                ),
+            DeleteAction::make(),
+        ];
+    }
+
+    public static function getBulkActions(): array
+    {
+        return [
+            BulkActionGroup::make([
+                DeleteBulkAction::make(),
+            ])
+        ];
+    }
+}
