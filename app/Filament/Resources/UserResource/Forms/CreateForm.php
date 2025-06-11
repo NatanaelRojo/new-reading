800b60535cb7fc7dada2a3c8f623e533e521b2ac
natanael <rojonatanael99@gmail.com>
2025-06-11 14:58:53 -0400

    feat(CreateForm): add tabs for user and author details in create form

--- app/Filament/Resources/UserResource/Forms/CreateForm.php
+++ app/Filament/Resources/UserResource/Forms/CreateForm.php
@@ -4,12 +4,35 @@ namespace App\Filament\Resources\UserResource\Forms;
 
 use App\Filament\Resources\Abstract\AbstractCreateForm;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 
 class CreateForm extends AbstractCreateForm
 {
     public static function make(): array
+    {
+        return [
+            Tabs::make('Tabs')
+                ->tabs(static::getTabs())
+                    ->activeTab(1),
+        ];
+    }
+
+    private static function getTabs(): array
+    {
+        return [
+            Tab::make('User Details')
+                ->schema(static::getFields()),
+            Tab::make('Author details')
+                ->schema(static::getAuthorFields()),
+        ];
+    }
+
+    private static function getFields(): array
     {
         return [
             TextInput::make('first_name')
@@ -31,6 +54,23 @@ class CreateForm extends AbstractCreateForm
     ->multiple()
     ->relationship('roles', 'name')
     ->preload(),
+        ];
+    }
+
+    public static function getAuthorFields(): array
+    {
+        return [
+            TextInput::make('author.first_name')
+                ->required(),
+            TextInput::make('author.last_name')
+                ->required(),
+            TextInput::make('author.nationality')
+                ->required(),
+            Textarea::make('author.biography')
+                ->required(),
+                FileUpload::make('author.image')
+                ->avatar()
+                ->imageEditor(),
         ];
     }
 }
