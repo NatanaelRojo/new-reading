71a1d0d828b1d921f567cbdde2a91d2e22d16732
natanael <rojonatanael99@gmail.com>
2025-06-11 10:41:53 -0400

    feat(AuthorFactory): add forUser method to associate Author with a User

--- database/factories/API/V1/AuthorFactory.php
+++ database/factories/API/V1/AuthorFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories\API\V1;
 
+use App\Models\API\V1\Author;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -23,5 +25,21 @@ class AuthorFactory extends Factory
             'biography' => fake()->text(),
             'image_url' => fake()->imageUrl(),
         ];
+    }
+
+    /**
+     * Configure the Author factory to associate a newly created User with the Author.
+     *
+     * This method uses the afterCreating callback to create a new User instance
+     * and updates the Author's user_id field with the created User's ID.
+     *
+     * @return static
+     */
+    public function forUser(): static
+    {
+        return $this->afterCreating(function (Author $author) {
+            $user = User::factory()->create();
+            $author->update(['user_id' => $user->id]);
+        });
     }
 }
