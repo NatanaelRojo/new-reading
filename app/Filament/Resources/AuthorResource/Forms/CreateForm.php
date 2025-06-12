026b8dac1cfd3cd901567a6551cd33d15e40ba77
natanael <rojonatanael99@gmail.com>
2025-06-11 21:53:44 -0400

    refactor(CreateForm): remove tab structure and user fields for simplification

--- app/Filament/Resources/AuthorResource/Forms/CreateForm.php
+++ app/Filament/Resources/AuthorResource/Forms/CreateForm.php
@@ -19,24 +19,6 @@ class CreateForm extends AbstractCreateForm
     public static function make(): array
     {
         return [
-            Tabs::make('Tabs')
-                ->tabs(static::getTabs()),
-        ];
-    }
-
-    private static function getTabs(): array
-    {
-        return [
-            Tab::make('Author Details')
-                ->schema(static::getFields()),
-            Tab::make('User details')
-                ->schema(static::getUserFields()),
-        ];
-    }
-
-    public static function getFields(): array
-    {
-        return [
             TextInput::make('first_name')
                 ->required(),
             TextInput::make('last_name')
@@ -48,31 +30,6 @@ class CreateForm extends AbstractCreateForm
                 FileUpload::make('image')
                 ->avatar()
                 ->imageEditor(),
-        ];
-    }
-
-    private static function getUserFields(): array
-    {
-        return  [
-            TextInput::make('user.first_name')
-                ->required(),
-            TextInput::make('user.last_name')
-                ->required(),
-                TextInput::make('user.email')
-                ->required()
-                ->email(),
-            TextInput::make('user.name')
-                ->required(),
-            TextInput::make('user.description')
-                ->required(),
-            TextInput::make('user.password')
-                ->required(),
-            DatePicker::make('user.birth_date')
-                ->required(),
-    //             Select::make('user.roles')
-    // ->multiple()
-    // ->relationship('roles', 'name')
-    // ->preload(),
         ];
     }
 }
