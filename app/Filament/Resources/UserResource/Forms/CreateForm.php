98c3c2d302822005a9be8831bca8b93ce1c66c1d
natanael <rojonatanael99@gmail.com>
2025-06-11 22:25:26 -0400

    refactor(CreateForm): simplify form structure by removing unused fields and methods

--- app/Filament/Resources/UserResource/Forms/CreateForm.php
+++ app/Filament/Resources/UserResource/Forms/CreateForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\UserResource\Forms;
 
 use App\Filament\Resources\Abstract\AbstractCreateForm;
+use App\Filament\Resources\AuthorResource\Forms\CreateForm as FormsCreateForm;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Section;
@@ -32,72 +33,6 @@ class CreateForm extends AbstractCreateForm
                 ->required(),
             DatePicker::make('birth_date')
                 ->required(),
-                Select::make('roles')
-    ->multiple()
-    ->relationship('roles', 'name')
-    ->preload(),
-    static::getAuthorSection(),
-        ];
-    }
-
-    private static function getTabs(): array
-    {
-        return [
-            Tab::make('User Details')
-                ->schema(static::getFields()),
-            Tab::make('Author details')
-            ->relationship('author')
-                ->schema(static::getAuthorFields()),
-        ];
-    }
-
-    private static function getFields(): array
-    {
-        return [
-            TextInput::make('first_name')
-                ->required(),
-            TextInput::make('last_name')
-                ->required(),
-                TextInput::make('email')
-                ->required()
-                ->email(),
-            TextInput::make('name')
-                ->required(),
-            TextInput::make('description')
-                ->required(),
-            TextInput::make('password')
-                ->required(),
-            DatePicker::make('birth_date')
-                ->required(),
-                Select::make('roles')
-    ->multiple()
-    ->relationship('roles', 'name')
-    ->preload(),
-        ];
-    }
-
-    public static function getAuthorSection(): Section
-    {
-        return Section::make('Author Detail')
-            ->relationship('author')
-                ->schema(static::getAuthorFields())
-                ->collapsible();
-    }
-
-    private static function getAuthorFields(): array
-    {
-        return [
-            TextInput::make('first_name')
-                ->required(),
-            TextInput::make('last_name')
-                ->required(),
-            TextInput::make('nationality')
-                ->required(),
-            Textarea::make('biography')
-                ->required(),
-                FileUpload::make('image')
-                ->avatar()
-                ->imageEditor(),
         ];
     }
 }
