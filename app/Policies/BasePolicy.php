0a8448d57628c22599e60e3b42e95eba32078d11
natanael <rojonatanael99@gmail.com>
2025-06-18 11:17:40 -0400

    fix(BasePolicy): update role checks to use configurable default roles for various actions

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -9,101 +9,122 @@ use Illuminate\Database\Eloquent\Model;
 abstract class BasePolicy
 {
     /**
-     * Determines if the user has any of the common roles allowed for general viewing.
-     *
-     * @return bool
-     */
-    protected function hasCommonViewAnyRoles(User $user): bool
-    {
-        return $user->hasAnyRole([
-            AppRoles::EDITOR,
-            AppRoles::AUTHOR,
-            AppRoles::MODERATOR,
-            AppRoles::USER,
-        ]);
-    }
-
-    /**
-     * Determines if the user has any of the common roles allowed for viewing a specific model.
-     *
-     * @return bool
-     */
-    protected function hasCommonViewRoles(User $user): bool
-    {
-        return $user->hasAnyRole([
-            AppRoles::EDITOR,
-            AppRoles::AUTHOR,
-            AppRoles::MODERATOR,
-            AppRoles::USER,
-        ]);
-    }
-
-    /**
-     * Determines if the user has common roles allowed for creating.
-     *
-     * @return bool
-     */
-    protected function hasCommonCreateRoles(User $user): bool
-    {
-        return $user->hasAnyRole([
-            AppRoles::ADMIN,
-        ]);
-    }
-
-    /**
-     * Determines if the user has common roles allowed for updating.
-     *
-     * @return bool
-     */
-    protected function hasCommonUpdateRoles(User $user): bool
-    {
-        return $user->hasAnyRole([
-            AppRoles::ADMIN,
-            AppRoles::EDITOR,
-            AppRoles::AUTHOR,
-        ]);
-    }
-
-    /**
-     * Determines if the user has common roles allowed for deleting.
+     * Determine if the user has any of the common roles allowed for general viewing.
+     * If specific roles are provided, it checks against those; otherwise, it checks against a default set defined in config.
      *
      * @param \App\Models\User $user The authenticated user.
+     * @param array $roles An optional array of roles to check against. If empty, a default set of roles is used from config.
      * @return bool
      */
-    protected function hasCommonDeleteRoles(User $user): bool
+    protected function hasCommonViewAnyRoles(User $user, array $roles = []): bool
     {
-        return $user->hasAnyRole([
-            AppRoles::ADMIN,
-            AppRoles::MODERATOR,
-        ]);
+        $rolesToCheck = !empty($roles)
+            ? $roles :
+            config('policies.default_roles.view_any', []);
+
+        return $user->hasAnyRole($rolesToCheck);
     }
 
     /**
-     * Determines if the user has common roles allowed for restoring.
+     * Determine if the user has any of the common roles allowed for viewing a specific model.
+     * If specific roles are provided, it checks against those; otherwise, it checks against a default set defined in config.
      *
      * @param \App\Models\User $user The authenticated user.
+     * @param array $roles An optional array of roles to check against. If empty, a default set of roles is used from config.
      * @return bool
      */
-    protected function hasCommonRestoreRoles(User $user): bool
+    protected function hasCommonViewRoles(User $user, array $roles = []): bool
     {
-        return $user->hasAnyRole([
-            AppRoles::ADMIN,
-            AppRoles::MODERATOR,
-        ]);
+        $rolesToCheck = !empty($roles) ?
+            $roles :
+            config('policies.default_roles.view', []);
+
+        return $user->hasAnyRole($rolesToCheck);
     }
 
     /**
-     * Determines if the user has common roles allowed for force deleting.
+     * Determine if the user has common roles allowed for creating.
+     * If specific roles are provided, it checks against those; otherwise, it checks against a default set defined in config.
      *
      * @param \App\Models\User $user The authenticated user.
+     * @param array $roles An optional array of roles to check against. If empty, a default set of roles is used from config.
      * @return bool
      */
-    protected function hasCommonForceDeleteRoles(User $user): bool
+    protected function hasCommonCreateRoles(User $user, array $roles = []): bool
     {
-        return $user->hasAnyRole([
-            AppRoles::ADMIN,
-            // AppRoles::MODERATOR,
-        ]);
+        $rolesToCheck = !empty($roles) ?
+            $roles :
+            config('policies.default_roles.create', []);
+
+        return $user->hasAnyRole($rolesToCheck);
+    }
+
+    /**
+     * Determine if the user has common roles allowed for updating.
+     * If specific roles are provided, it checks against those; otherwise, it checks against a default set defined in config.
+     *
+     * @param \App\Models\User $user The authenticated user.
+     * @param array $roles An optional array of roles to check against. If empty, a default set of roles is used from config.
+     * @return bool
+     */
+    protected function hasCommonUpdateRoles(User $user, array $roles = []): bool
+    {
+        $rolesToCheck = !empty($roles) ?
+            $roles :
+            config('policies.default_roles.update', []);
+
+        return $user->hasAnyRole($rolesToCheck);
+    }
+
+    /**
+     * Determine if the user has common roles allowed for deleting.
+     * If specific roles are provided, it checks against those; otherwise, it checks against a default set defined in config.
+     *
+     * @param \App\Models\User $user The authenticated user.
+     * @param array $roles An optional array of roles to check against. If empty, a default set of roles is used from config.
+     * @return bool
+     */
+    protected function hasCommonDeleteRoles(User $user, array $roles = []): bool
+    {
+        $rolesToCheck = !empty($roles) ?
+            $roles :
+            config('policies.default_roles.delete', []);
+
+        return $user->hasAnyRole($rolesToCheck);
+    }
+
+    /**
+     * Determine if the user has common roles allowed for restoring.
+     * If specific roles are provided, it checks against those; otherwise, it checks against a default set defined in config.
+     *
+     * @param \App\Models\User $user The authenticated user.
+     * @param array $roles An optional array of roles to check against. If empty, a default set of roles is used from config.
+     * @return bool
+     */
+    protected function hasCommonRestoreRoles(User $user, array $roles = []): bool
+    {
+        $rolesToCheck = !empty($roles) ?
+            $roles :
+            config('policies.default_roles.restore', []);
+
+        return $user->hasAnyRole($rolesToCheck);
+    }
+
+    /**
+     * Determine if the user has common roles allowed for force deleting.
+     * If specific roles are provided, it checks against those; otherwise, it checks against a default set defined in config.
+     *
+     * @param \App\Models\User $user The authenticated user.
+     * @param array $roles An optional array of roles to check against. If empty, a default set of roles is used from config.
+     * @return bool
+     */
+    protected function hasCommonForceDeleteRoles(User $user, array $roles = []): bool
+    {
+        $rolesToCheck = !empty($roles) ?
+            $roles :
+            config('policies.default_roles.force_delete', []);
+
+        return $user->hasAnyRole($rolesToCheck);
     }
 
     /**
